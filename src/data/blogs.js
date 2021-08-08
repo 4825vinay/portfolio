16afd6adf0c989fcba24bb1b6f8d46e6636b9f98
4825vinay <f20190788@goa.bits-pilani.ac.in>
2021-08-08 21:14:55 +0530

    I did it

---
+++ src/data/blogs.js
@@ -0,0 +1,38 @@
+import blog1 from "../img/blogs/blog1.svg";
+import blog3 from "../img/blogs/blog3.svg";
+import blog2 from "../img/blogs/blog2.svg";
+import blog4 from "../img/blogs/blog4.svg";
+
+const blogs = [
+  {
+    id: 1,
+    title: "A layer of trust on the Internet",
+    date: "01",
+    month: "April",
+    image: blog1,
+  },
+  {
+    id: 2,
+    title: "How a mobile Identity wallet can change your life",
+    date: "01",
+    month: "April",
+    image: blog2,
+  },
+  {
+    id: 3,
+    title: "The future of Decentralized Applications",
+    date: "01",
+    month: "April",
+    image: blog3,
+    link: "https://www.google.co.uk/",
+  },
+  {
+    id: 4,
+    title: "DAO's - Decentralized autonomous organizations",
+    date: "01",
+    month: "April",
+    image: blog4,
+  },
+];
+
+export default blogs;
---
+++ src/data/portfolios.js
@@ -0,0 +1,112 @@
+import css1 from "../img/portImages/cv_css.png";
+import css2 from "../img/portImages/ms.png";
+import react1 from "../img/portImages/react_styled.png";
+import img8 from "../img/portImages/pro7.png";
+import img3 from "../img/portImages/maya-3.png";
+import img5 from "../img/portImages/proj3.png";
+import img6 from "../img/portImages/proj4.png";
+import img7 from "../img/portImages/proj5.png";
+import img9 from "../img/portImages/pro8.png";
+import img10 from "../img/portImages/pro9.png";
+import img11 from "../img/portImages/pro10.png";
+
+const portfolios = [
+  {
+    id: 4,
+    category: "Blockchain",
+    image: img6,
+    link1: "https://www.google.com",
+    link2: "https://www.google.com",
+    title: " Blockchain ",
+    text: "With POS consensus",
+  },
+
+  {
+    id: 5,
+    category: "Blockchain",
+    image: img7,
+    link1: "https://www.google.com",
+    link2: "https://www.google.com",
+    title: "Solidity Smart Contracts",
+    text: "Free Animation Software",
+  },
+
+  {
+    id: 3,
+    category: "Web Development",
+    image: img5,
+    link1: "https://www.google.com",
+    link2: "https://www.google.com",
+    title: "Startup-Landing page",
+    text: "React Js & styled components",
+  },
+
+  {
+    id: 10,
+    category: "Blockchain",
+    image: img11,
+    link1: "https://www.google.com",
+    link2: "https://www.google.com",
+    title: "IPFS projects",
+    text: "Interplanetary file storage",
+  },
+
+  {
+    id: 9,
+    category: "Blockchain",
+    image: img10,
+    link1: "https://www.google.com",
+    link2: "https://www.google.com",
+    title: "GETH Test network",
+    text: "using ganache cli",
+  },
+
+  {
+    id: 1,
+    category: "Machine learning",
+    image: css1,
+    link1: "https://www.google.com",
+    link2: "https://www.google.com",
+    title: "Machine learning Project",
+    text: "Regression and Deep learning models",
+  },
+  {
+    id: 2,
+    category: "Machine learning",
+    image: img3,
+    link1: "https://www.google.com",
+    link2: "https://www.google.com",
+    title: "Facial Recognition",
+    text: " Opencv and python",
+  },
+
+  {
+    id: 6,
+    category: "Web Development",
+    image: img8,
+    link1: "https://www.google.com",
+    link2: "https://www.google.com",
+    title: "Cryptocurrency blog",
+    text: "Financial Research Reporter",
+  },
+  {
+    id: 7,
+    category: "Web Development",
+    image: css2,
+    link1: "https://www.google.com",
+    link2: "https://www.google.com",
+    title: "Microsoft Website Clone",
+    text: "Created using HTML and CSS",
+  },
+  {
+    id: 8,
+    category: "ROS",
+    image: img9,
+    link1: "https://www.google.com",
+    link2: "https://www.google.com",
+    title: "Rusky Bot simulation",
+    text: "In Gazebo physics engine ( ROS )",
+  },
+];
+
+export default portfolios;
